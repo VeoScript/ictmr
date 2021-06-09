009fedc2480c887e428b35ebe31bc87797c2a35e
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-09 11:06:37 +0800

    refactor: change js to jsx (#3)

--- src/layout/default.js
+++
@@ -1,9 +0,0 @@
-export default function Layout({ children }) {
-  return (
-    <div className="font-poppins flex flex-row w-full h-screen mx-auto transition ease-in-out duration-700 overflow-y-auto">
-      <div className="flex w-full h-auto">
-        { children }
-      </div>
-    </div>
-  )
-}
