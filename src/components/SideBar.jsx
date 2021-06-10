75c7e25fe19d3f7ec5c6014e18df3b383b80d4a1
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-10 11:08:21 +0800

    feat: add react-tooltip (#16)

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,12 +1,14 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { menu } from '~/static/links'
+import ReactTooltip from 'react-tooltip'
 
 export default function SideBar() {
   const router = useRouter()
   return (
     <div className="flex flex-col w-full h-full overflow-y-auto bg-light-panther">
-      <div className="flex flex-row justify-center items-center w-full h-20">
+      <ReactTooltip />
+      <div className="flex flex-col justify-center items-center w-full h-20">
         <Link href="/">
           <a className="transition ease-in-out duration-300 transform hover:scale-90">
             <img className="w-8 h-8" src="./ictmr.png" alt="icon" />
@@ -14,9 +16,9 @@ export default function SideBar() {
         </Link>
       </div>
       <div className="flex flex-col justify-center items-center w-full h-full space-y-5">
-        {menu.map(({ icon, href }, i) => (
+        {menu.map(({ name, icon, href }, i) => (
           <Link href={ href } key={i}>
-            <a className={`${router.pathname == href ? 'bg-scarlet' : 'bg-light-panther'} flex flex-row justify-center w-1/2 px-5 py-2 rounded-md transition ease-in-out duration-300 transform hover:scale-90`}>
+            <a data-tip={ name } className={`${router.pathname == href ? 'bg-scarlet' : 'bg-light-panther'} flex flex-row justify-center w-1/2 px-5 py-2 rounded-md transition ease-in-out duration-300 transform hover:scale-90`}>
               <span>{ icon }</span>
             </a>
           </Link>
