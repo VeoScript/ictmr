09510b6c5e9264849369009c237f5f05230a281f
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-10 14:30:06 +0800

    chore: change layouts of default layout (#20)

--- src/layout/default.jsx
+++ src/layout/default.jsx
@@ -1,8 +1,17 @@
+import SideBar from '~/components/SideBar'
+
 export default function Layout({ children }) {
   return (
     <div className="font-poppins flex flex-row w-full h-screen mx-auto transition ease-in-out duration-700 overflow-y-auto bg-panther text-bright-white">
       <div className="flex w-full h-auto">
-        { children }
+        <div className="flex flex-row justify-between w-full h-full">
+          <div className="flex flex-col w-full max-w-[4rem] h-full overflow-hidden">
+            <SideBar />
+          </div>
+          <div className="flex flex-col items-center w-full max-w-full h-full overflow-y-auto">
+            { children }
+          </div>
+        </div>
       </div>
     </div>
   )
