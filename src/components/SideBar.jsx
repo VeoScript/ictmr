fe2dc47b15b20b9af5fc451a43e2d32efadf6d62
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-10 11:46:59 +0800

    refactor: remove react-tooltip (#17)
    * refactor: delete react-tooltip
    
    * chore: set div to a tag

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,13 +1,11 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { menu } from '~/static/links'
-import ReactTooltip from 'react-tooltip'
 
 export default function SideBar() {
   const router = useRouter()
   return (
     <div className="flex flex-col w-full h-full overflow-y-auto bg-light-panther">
-      <ReactTooltip />
       <div className="flex flex-col justify-center items-center w-full h-20">
         <Link href="/">
           <a className="transition ease-in-out duration-300 transform hover:scale-90">
@@ -16,9 +14,9 @@ export default function SideBar() {
         </Link>
       </div>
       <div className="flex flex-col justify-center items-center w-full h-full space-y-5">
-        {menu.map(({ name, icon, href }, i) => (
+        {menu.map(({ icon, href }, i) => (
           <Link href={ href } key={i}>
-            <a data-tip={ name } className={`${router.pathname == href ? 'bg-scarlet' : 'bg-light-panther'} flex flex-row justify-center w-1/2 px-5 py-2 rounded-md transition ease-in-out duration-300 transform hover:scale-90`}>
+            <a className={`${router.pathname == href ? 'bg-scarlet' : 'bg-light-panther'} flex flex-row justify-center w-1/2 px-5 py-2 rounded-md transition ease-in-out duration-300 transform hover:scale-90`}>
               <span>{ icon }</span>
             </a>
           </Link>
