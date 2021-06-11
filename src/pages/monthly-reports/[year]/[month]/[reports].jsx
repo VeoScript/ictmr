071dac87f3ba555802403774af9b14de549042ef
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-11 11:04:19 +0800

    chore: update dynamic routes url links

--- src/pages/monthly-reports/[year]/[month]/[reports].jsx
+++ src/pages/monthly-reports/[year]/[month]/[reports].jsx
@@ -1,20 +1,18 @@
 import { useRouter } from 'next/router'
 
-export default function Reports() {
+export default function Month() {
   const router = useRouter()
   const { year, month, reports } = router.query
 
   return (
     <div className="flex flex-col justify-center items-center w-full h-full">
       <div className="py-5 font-light text-5xl">
-        Year: <span className="font-bold text-5xl">{ year }</span>
-      </div>
-      <div className="py-5 font-light text-5xl">
-        Month: <span className="font-bold text-5xl">{ month }</span>
-      </div>
-      <div className="py-5 font-light text-5xl">
-        Reports: <span className="font-bold text-5xl">{ reports }</span>
+        Reports from the month and year of &nbsp;
+        <span className="font-bold">{ month }</span>,&nbsp;
+        <span className="font-bold">{ year }</span> :
+        <span className="font-bold text-5xl">{ reports }</span>
       </div>
     </div>
   )
 }
+
--- src/pages/monthly-reports/[year]/[month]/index.jsx
+++ src/pages/monthly-reports/[year]/[month]/index.jsx
@@ -1,14 +1,23 @@
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
-export default function Month() {
+export default function Reports() {
   const router = useRouter()
-  const { year, month, reports } = router.query
+  const { year, month } = router.query
 
   return (
     <div className="flex flex-col justify-center items-center w-full h-full">
       <div className="py-5 font-light text-5xl">
-        Reports from the month and year of { year } { month }: <span className="font-bold text-5xl">{ reports }</span>
+        Year: <span className="font-bold text-5xl">{ year }</span>
       </div>
+      <div className="py-5 font-light text-5xl">
+        Month: <span className="font-bold text-5xl">{ month }</span>
+      </div>
+      <Link href="/monthly-reports/[year]/[month]/[reports]" as={`/monthly-reports/${year}/${month}/reports-args`}>
+        <a className="py-5 font-light text-5xl">
+          See reports from the month of { month }, { year }
+        </a>
+      </Link>
     </div>
   )
 }
