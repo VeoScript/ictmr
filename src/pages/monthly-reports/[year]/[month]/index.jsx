e97d0591c451c84f542f876a0206814cd19ec5a5
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-11 11:30:51 +0800

    refactor: delete [reports].jsx route

--- src/pages/monthly-reports/[year]/[month]/index.jsx
+++ src/pages/monthly-reports/[year]/[month]/index.jsx
@@ -1,23 +1,12 @@
 import { useRouter } from 'next/router'
-import Link from 'next/link'
 
 export default function Reports() {
   const router = useRouter()
   const { year, month } = router.query
 
   return (
-    <div className="flex flex-col justify-center items-center w-full h-full">
-      <div className="py-5 font-light text-5xl">
-        Year: <span className="font-bold text-5xl">{ year }</span>
-      </div>
-      <div className="py-5 font-light text-5xl">
-        Month: <span className="font-bold text-5xl">{ month }</span>
-      </div>
-      <Link href="/monthly-reports/[year]/[month]/[reports]" as={`/monthly-reports/${year}/${month}/reports-args`}>
-        <a className="py-5 font-light text-5xl">
-          See reports from the month of { month }, { year }
-        </a>
-      </Link>
+    <div className="flex flex-col justify-center items-center w-full h-screen">
+     <h1>All reports from the month of <span className="font-bold text-2xl uppercase">{ month }</span>, <span className="font-bold text-2xl">{ year }</span></h1>
     </div>
   )
 }
