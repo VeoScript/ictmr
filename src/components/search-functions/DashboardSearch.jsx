a0c9c96bd91f9dd7d2e94545aac569a0917a0cd1
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-13 21:29:30 +0800

    chore: change search-route input-text placeholder name

--- src/components/search-functions/DashboardSearch.jsx
+++ src/components/search-functions/DashboardSearch.jsx
@@ -38,7 +38,7 @@ export default function DashboardSearch() {
           name="search"
           value={searchTerm}
           onChange={handleChange}
-          placeholder="Search"
+          placeholder="Search Route"
         />
         <svg className="w-6 h-6 text-gray-400 mr-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
