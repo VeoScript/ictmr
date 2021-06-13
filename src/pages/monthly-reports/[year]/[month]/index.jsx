6acd08b7bb1746c783d46f1ac53a39abea3955d7
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-13 15:59:57 +0800

    feat: display all reports by year and months (#43)

--- src/pages/monthly-reports/[year]/[month]/index.jsx
+++ src/pages/monthly-reports/[year]/[month]/index.jsx
@@ -1,12 +1,66 @@
-import { useRouter } from 'next/router'
+import Head from 'next/head'
+import SearchBar from '~/components/search-functions/monthly-reports/SearchMonth'
+import DisplayReports from '~/components/DisplayReports'
+import { PrismaClient } from '@prisma/client'
 
-export default function Reports() {
-  const router = useRouter()
-  const { year, month } = router.query
+const prisma = new PrismaClient()
 
+export default function Reports({ getYear, getMonth, reports }) {
   return (
-    <div className="flex flex-col justify-center items-center w-full h-screen">
-     <h1>All reports from the month of <span className="font-bold text-2xl uppercase">{ month }</span>, <span className="font-bold text-2xl">{ year }</span></h1>
-    </div>
+    <>
+      <Head>
+        <title>Reports | { getMonth.month }, { getYear.year } </title>
+      </Head>
+      <div className="flex flex-col items-center w-full max-w-full h-screen overflow-y-auto bg-panther text-bright-white">
+        <div className="flex flex-row justify-between items-center w-full px-14 mt-14">
+          <div className="flex flex-col items-start w-full space-y-1">
+            <h1 className="font-bold text-5xl">My Report</h1>
+          </div>
+          <div className="flex flex-col w-full">
+            <SearchBar />
+          </div>
+          <div className="flex flex-col items-end w-full">
+            <h1 className="font-bold text-5xl">{ getMonth.month }&nbsp;{ getYear.year }</h1>
+          </div>
+        </div>
+        <div className="flex flex-col w-full h-full px-10 py-10 mt-5">
+          <DisplayReports getReports={ reports } />
+        </div>
+      </div>
+    </>
   )
+}
+
+export async function getServerSideProps(context) {
+  const { year } = context.query
+  const { month } = context.query
+  const getYear = await prisma.yearAlbum.findFirst({
+    where: {
+      year: year
+    }
+  })
+  const getMonth = await prisma.monthAlbum.findFirst({
+    where: {
+      albumYear: year,
+      month: month
+    }
+  })
+  const reports = await prisma.reports.findMany({
+    orderBy: [
+      {
+        id: 'asc'
+      }
+    ],
+    where: {
+      albumYear: year,
+      albumMonth: month
+    }
+  })
+  return {
+    props: {
+      getYear,
+      getMonth,
+      reports
+    }
+  }
 }
