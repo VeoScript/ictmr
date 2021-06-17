6d4775e940780c225aa2ff37c6ffc7f877954227
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-17 18:36:39 +0800

    feat: add notes search-bar component (#99)

--- src/components/DisplayNotes.jsx
+++ src/components/DisplayNotes.jsx
@@ -9,7 +9,7 @@ export default function DisplayYear({ notes }) {
         return (
           <Link href="/notes/[slug]" as={`/notes/${slug}`} key={i}>
             <a className="flex flex-col w-full py-5">
-              <div className="flex flex-col h-[21rem] bg-light-panther text-bright-white rounded-xl transition ease-in-out duration-300 transform hover:scale-95 hover:shadow-md">
+              <div className="flex flex-col h-[23rem] bg-light-panther text-bright-white rounded-xl transition ease-in-out duration-300 transform hover:scale-95 hover:shadow-md">
                 <div className="relative">
                   <div className="absolute w-full inset-0 -top-5 z-30">
                     <div className="flex flex-row justify-center w-full">
@@ -21,12 +21,12 @@ export default function DisplayYear({ notes }) {
                   <div className="absolute w-full inset-0 top-44 mt-5 px-6 z-10">
                     <div className="flex flex-col w-full space-y-2 px-3">
                       <div className="flex flex-row justify-between w-full">
-                        <div className="flex flex-col w-full">
+                        <div className="flex flex-col">
                           <span className="font-bold text-xl">{ title }</span>
                           <span className="font-light text-xs text-cool-gray"><Moment date={ date } format='LL' /></span>
                         </div>
-                        <div className="flex flex-row items-center justify-end w-full">
-                          <span className="font-bold text-sm bg-scarlet text-light-panther px-3 py-1 rounded-full">{ tag }</span>
+                        <div className="flex flex-row items-start justify-end">
+                          <span className="font-bold text-sm bg-scarlet text-panther px-3 py-1 rounded-full">{ tag }</span>
                         </div>
                       </div>
                       <div className="flex flex-col w-full space-y-3">
---
+++ src/components/search-functions/NoteSearch.jsx
@@ -0,0 +1,80 @@
+import Link from 'next/link'
+import { useState } from 'react'
+import Scrollbar from 'react-smooth-scrollbar'
+
+export default function NoteSearch({ notes }) {
+  const [searchTerm, setSearchTerm] = useState("")
+  const [isOpen, setIsOpen] = useState("")
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value)
+    if(!event.target.value){
+      setIsOpen(false)
+    }
+    else{
+      setIsOpen(true)
+    }
+  }
+
+  const getNote = notes.map(({ image, title, slug }, counter) => {
+    return [
+      title,
+      image,
+      slug,
+      counter
+    ]
+  })
+
+  const results = !searchTerm ? getNote : getNote.filter(note =>
+    note[0].toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase())
+  )
+
+  return (
+    <div className="flex flex-col w-full">
+      <div className="searchbox flex flex-row items-center w-full max-w-full bg-light-panther rounded-full">
+        <input
+          className="w-full px-5 py-3 bg-light-panther rounded-full focus:outline-none"
+          type="text"
+          name="search"
+          value={searchTerm}
+          onChange={handleChange}
+          placeholder="Search"
+        />
+        <svg className="w-6 h-6 text-gray-400 mr-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+        </svg>
+      </div>
+      {setIsOpen && (
+        <>
+          <button onClick={(e) => {setIsOpen(false); setSearchTerm(e.target.value="")}} type="button" className={`${isOpen ? 'z-20 block fixed inset-0 w-full h-full cursor-default focus:outline-none' : 'hidden'}`}></button>
+          <div className={`z-40 w-full max-w-sm h-full ${isOpen ? 'relative' : 'hidden'}`}>
+            <div className="absolute w-full h-auto overflow-hidden mt-2 rounded-xl bg-panther border-2 border-scarlet text-white z-10">
+              <div className="flex flex-row w-full h-auto max-h-[15rem] overflow-y-auto bg-opacity-75">
+                <Scrollbar
+                  className="w-full"
+                  damping={0.1}
+                  thumbMinSize={20}
+                >
+                  <ul className="flex flex-col w-full">
+                    {results.map(note => {
+                      return (
+                        <li className="flex flex-row items-center w-full border-b border-light-panther" key={note[3]}>
+                          <Link href="/notes/[slug]" as={`/notes/${note[2]}`}>
+                            <a className="flex flex-row items-center w-full px-3 py-2 transition ease-in-out duration-200 hover:bg-light-panther space-x-3">
+                              <img className="w-12 h-12 rounded-full object-cover" src={note[1]} alt="avatar" />
+                              <span className="font-normal text-lg text-scheme-pale">{note[0]}</span>
+                            </a>
+                          </Link>
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </Scrollbar>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  )
+}
