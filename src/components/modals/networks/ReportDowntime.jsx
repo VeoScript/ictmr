3179f179c07689d093e231772cee829e65f957b7
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-17 14:11:17 +0800

    chore: change svg icon

--- src/components/modals/networks/ReportDowntime.jsx
+++ src/components/modals/networks/ReportDowntime.jsx
@@ -140,7 +140,7 @@ export default function ReportDowntime() {
                           placeholder="Description"
                         />
                         <svg className="w-6 h-6 text-gray-400 mr-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"></path>
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
                         </svg>
                       </div>
                       { errors.description && <span className="font-medium text-xs tracking-wide text-bright-white mx-5">Description is required.</span> }
