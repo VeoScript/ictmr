84eab77a012caa672d62834170c782bd1e2edc1d
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-18 11:57:48 +0800

    style: change notes slug page layout

--- src/pages/notes/[slug].jsx
+++ src/pages/notes/[slug].jsx
@@ -28,10 +28,10 @@ export default function ComputerID({ notes }) {
       </Head>
       <Layout>
         <Scrollbar
-          className="w-full"
+          className="w-full h-full"
         >
           <div className="flex flex-col items-center w-full h-full px-10 py-10 space-y-5">
-            <div className="flex flex-row w-full max-w-5xl">
+            <div className="flex flex-row w-full max-w-6xl">
               <div className="flex flex-col w-full">
                 <h1 className="font-bold text-3xl text-bright-white">{ notes.title }</h1>
                 <h6 className="font-light text-xl text-bright-white">{ notes.tag }</h6>
@@ -41,18 +41,22 @@ export default function ComputerID({ notes }) {
                 <DeleteNotes notes={ notes } />
               </div>
             </div>
-            <div className="flex flex-row w-full h-[30rem] max-w-5xl">
+            <div className="flex flex-row w-full h-[30rem] max-w-6xl">
               <img className="w-full h-full rounded-2xl shadow-2xl object-cover" src={ notes.image } alt="cover-photo" />
             </div>
-            <div className="flex flex-col w-full max-w-5xl py-10 px-10 rounded-2xl space-y-3 bg-bright-white">
-              <article className="font-sans text-xl prose lg:prose-xl">
-                <ReactMarkdown>{ notes.note }</ReactMarkdown>
-              </article>
-              <div className="flex flex-col items-end w-full mt-10">
-                <span className="font-bold text-xl text-light-panther">{ notes.title }</span>
-                <span className="font-light text-base text-light-panther">
-                  <Moment date={ notes.date } format="llll" />
-                </span>
+            <div className="relative flex flex-col items-center w-full h-full">
+              <div className="absolute -top-60 flex flex-col items-center w-full">
+                <div className="flex flex-col w-full max-w-4xl py-10 px-10 mb-10 rounded-2xl space-y-3 opacity-90 bg-bright-white">
+                  <article className="font-sans text-xl prose lg:prose-xl">
+                    <ReactMarkdown>{ notes.note }</ReactMarkdown>
+                  </article>
+                  <div className="flex flex-col items-end w-full pt-10">
+                    <span className="font-bold text-xl text-light-panther">{ notes.title }</span>
+                    <span className="font-light text-base text-light-panther">
+                      <Moment date={ notes.date } format="llll" />
+                    </span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
