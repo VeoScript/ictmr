9e00bf344911fb1bd01f7eac61622beb3533e38b
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-22 08:16:37 +0800

    Revert "style: change progress-bar color (#118)"
    This reverts commit 2bd49af1b4600431381e6ae6d48603dcf8f0b26b.

--- src/lib/nprogress.js
+++ src/lib/nprogress.js
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 /* eslint-disable react/prefer-stateless-function */
 class NextNProgress extends React.Component {
   static defaultProps = {
-    color: '#00A3E1',
+    color: '#F8485E',
     startPosition: 0.3,
     stopDelayMs: 200,
     height: 3,
