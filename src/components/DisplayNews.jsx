a267bd58bb1b5b416805ee94b18c84a96ad32796
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2021-06-24 17:11:44 +0800

    feat: change news api in philippines top headline news (#129)

--- src/components/DisplayNews.jsx
+++ src/components/DisplayNews.jsx
@@ -4,14 +4,14 @@ import Moment from 'react-moment'
 export default function DisplayNews({ news }) {
   return (
     <>
-      {news.results.map((({ section, title, abstract, url, byline, published_date, multimedia }, i) => {
+      {news.articles.map((({ source, author, title, description, url, urlToImage, publishedAt, content }, i) => {
         return (
           <Link href={url}>
             <a target="_blank" className="flex flex-col h-[27rem] bg-light-panther text-bright-white mt-10 rounded-xl transition ease-in-out duration-300 transform hover:scale-95 hover:shadow-md" key={i}>
               <div className="relative">
                 <div className="absolute w-full inset-0 -top-5 z-30">
                   <div className="flex flex-row justify-center w-full">
-                    <img className="w-10/12 h-48 rounded-lg bg-cool-gray object-cover" src={ multimedia[0].url } alt="note-image" />
+                    <img className="w-10/12 h-48 rounded-lg bg-cool-gray object-cover" src={ !urlToImage ? 'https://blog.remitly.com/wp-content/uploads/2018/06/philippines-1195394_1920.jpg' : urlToImage } alt="news-image" />
                   </div>
                 </div>
               </div>
@@ -21,16 +21,16 @@ export default function DisplayNews({ news }) {
                     <div className="flex flex-row justify-start w-full">
                       <div className="flex flex-col">
                         <span className="font-semibold text-base">{ title }</span>
-                        <span className="font-light text-xs text-cool-gray"><Moment date={ published_date } format='LL' /></span>
+                        <span className="font-light text-xs text-cool-gray"><Moment date={ publishedAt } format='LL' /></span>
                       </div>
                     </div>
                     <div className="flex flex-col w-full space-y-3">
                       <span className="font-light text-sm line-clamp-2">
-                        {!abstract ? 'No abstract available' : abstract}
+                        {!content ? 'No content available' : content}
                       </span>
-                      <div className="flex flex-row w-full items-center space-x-1">
-                        <span className="font-normal text-xs text-cool-gray">{ byline }</span>
-                        <span className="font-normal text-xs text-cool-gray uppercase">- { section }</span>
+                      <div className="flex flex-col w-full items-center pt-1 space-y-1">
+                        <span className="font-normal text-xs text-cerulean uppercase">{ author }</span>
+                        <span className="font-bold text-xs text-cool-gray">{ source.name }</span>
                       </div>
                       <hr className="opacity-10" />
                     </div>
